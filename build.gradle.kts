1b893e119f0b675ab8d82c290bc4dbbfe95c6c96
konfuzius <raphael.goetz.7@gmail.com>
2023-11-18 17:10:13 +0100

    feat: added main class and gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    `java-library`
+}
+
+allprojects {
+    group = "com.uroria"
+    version = "0.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+}
+
+dependencies {
+    implementation("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
---
+++ src/main/java/com/uroria/betterflowers/BetterFlowers.java
@@ -0,0 +1,27 @@
+package com.uroria.betterflowers;
+
+import com.uroria.betterflowers.commands.Flowers;
+import com.uroria.betterflowers.commands.UndoFlower;
+import com.uroria.betterflowers.listeners.CustomFlowerPlaceListener;
+import com.uroria.betterflowers.managers.FlowerManager;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Getter
+public final class BetterFlowers extends JavaPlugin {
+
+    private final FlowerManager flowerManager;
+
+    public BetterFlowers() {
+        this.flowerManager = new FlowerManager();
+    }
+
+    @Override
+    public void onEnable() {
+
+        getCommand("flower").setExecutor(new Flowers(this));
+        getCommand("uf").setExecutor(new UndoFlower(this));
+        Bukkit.getPluginManager().registerEvents(new CustomFlowerPlaceListener(this), this);
+    }
+}
