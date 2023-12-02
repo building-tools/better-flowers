03040d21d921e91f0359e8a10c7e056d3e3c77f9
konfuzius <raphael.goetz.7@gmail.com>
2023-12-02 17:24:34 +0100

    feat: add air-setting to brush-menu

--- src/main/java/com/uroria/betterflowers/menus/FlowerBrushMenu.java
+++ src/main/java/com/uroria/betterflowers/menus/FlowerBrushMenu.java
@@ -9,6 +9,7 @@ import com.uroria.betterflowers.utils.ItemBuilder;
 
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -22,15 +23,17 @@ public final class FlowerBrushMenu extends BukkitPlayerInventory {
     private final Player player;
     private final List<FlowerGroupData> flowerGroupData;
     private int radius;
+    private float airRandomizer;
 
     public FlowerBrushMenu(BetterFlowers betterFlowers, Player player) {
-        super(MiniMessage.miniMessage().deserialize("<gradient:#232526:#414345>Flower Brush"), 3);
+        super(MiniMessage.miniMessage().deserialize("<gradient:#232526:#414345>Flower Brush"), 4);
 
         this.flowerManager = betterFlowers.getFlowerManager();
         this.player = player;
 
         this.flowerGroupData = new ArrayList<>();
         this.radius = 3;
+        this.airRandomizer = 0.0f;
 
         generateOverlay();
     }
@@ -43,8 +46,13 @@ public final class FlowerBrushMenu extends BukkitPlayerInventory {
 
         this.setSlot(0, new ItemBuilder(Material.ECHO_SHARD).setName("Create").build(), this::onCreateBrush);
         this.setSlot(1, new ItemBuilder(Material.ENDER_EYE).setName("Current radius: " + radius).build(), this::onChangeRadius);
+        this.setSlot(2, new ItemBuilder(Material.STRUCTURE_VOID).setName("Current air percentage" + airRandomizer).build(), this::onChangeAirRandomizer);
 
         for (int index = 18; index < 27; index++) {
+            this.setSlot(index, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName("Insert Flower Mask!").build(), inventoryClickEvent -> inventoryClickEvent.setCancelled(true));
+        }
+
+        for (int index = 27; index < 36; index++) {
             this.setSlot(index, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName("Insert Flower Placer!").build(), this::displayFlowerPlacer);
         }
     }
@@ -78,14 +86,24 @@ public final class FlowerBrushMenu extends BukkitPlayerInventory {
         this.setSlot(slot, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName(" ").build(), this::displayFlowerPlacer);
     }
 
+    private void onChangeAirRandomizer(InventoryClickEvent inventoryClickEvent) {
+        inventoryClickEvent.setCancelled(true);
+
+        if (inventoryClickEvent.isLeftClick() && airRandomizer > 1f) airRandomizer += 0.1f;
+        if (inventoryClickEvent.isRightClick() && airRandomizer < 0f) airRandomizer -= 0.1f;
+        if (airRandomizer > 1f) airRandomizer = 1f;
+        if (radius < 0f) airRandomizer = 0f;
+
+        this.setSlot(2, new ItemBuilder(Material.STRUCTURE_VOID).setName("Current air percentage" + airRandomizer).build(), this::onChangeAirRandomizer);
+    }
+
     private void onChangeRadius(InventoryClickEvent inventoryClickEvent) {
         inventoryClickEvent.setCancelled(true);
 
-        if (radius <= 0 || radius >= 51) return;
         if (inventoryClickEvent.isLeftClick()) radius += 1;
         if (inventoryClickEvent.isRightClick()) radius -= 1;
-        if (inventoryClickEvent.isLeftClick() && inventoryClickEvent.isShiftClick()) radius += 5;
-        if (inventoryClickEvent.isRightClick() && inventoryClickEvent.isShiftClick()) radius -= 5;
+        if (radius > 20) radius = 20;
+        if (radius < 1) radius = 1;
 
         this.setSlot(1, new ItemBuilder(Material.ENDER_EYE).setName("Current radius: " + radius).build(), this::onChangeRadius);
     }
@@ -98,7 +116,7 @@ public final class FlowerBrushMenu extends BukkitPlayerInventory {
 
         final var name = "<green>ID: " + System.currentTimeMillis() + "</green>";
         final var brushItem = new ItemBuilder(Material.BLAZE_ROD).setName(name).build();
-        this.flowerManager.getBrushes().put(brushItem, new BrushData(flowerGroupData, radius));
+        this.flowerManager.getBrushes().put(brushItem, new BrushData(flowerGroupData, radius, airRandomizer));
 
         this.player.getInventory().addItem(brushItem);
         this.player.sendMessage(MiniMessage.miniMessage().deserialize("<gradient:#a8ff78:#78ffd6>Flower has been created"));
